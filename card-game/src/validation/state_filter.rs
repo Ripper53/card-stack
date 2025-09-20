c2d7b2dbe87494ea16fd3c007bdaa349ff69f368
Albar <GalacticHyperStar@gmail.com>
2025-09-19 22:34:44 -0400

    feat!: impl for state filter tuple of 3

--- card-game/src/validation/state_filter.rs
+++ card-game/src/validation/state_filter.rs
@@ -53,7 +53,7 @@ where
             })
     }
 }
-/*impl<
+impl<
     State,
     F0: StateFilter<State>,
     F1: StateFilter<State>,
@@ -62,15 +62,37 @@ where
 > StateFilter<State> for (F0, F1, F2, F3)
 where
     F0::ValidOutput: StateFilterInput<F1::Input>,
-    F1::ValidOutput: StateFilterInput<F2::Input>
-        + StateFilterCombination<<F0::ValidOutput as StateFilterInput<F1::Input>>::Remainder>,
-    F2::ValidOutput: StateFilterInput<F3::Input>
-        + StateFilterCombination<<F1::ValidOutput as StateFilterInput<F2::Input>>::Remainder>,
-    F3::ValidOutput:
-        StateFilterCombination<<F2::ValidOutput as StateFilterInput<F3::Input>>::Remainder>,
+    F1::ValidOutput:
+        StateFilterCombination<<F0::ValidOutput as StateFilterInput<F1::Input>>::Remainder>,
+    <F1::ValidOutput as StateFilterCombination<
+        <F0::ValidOutput as StateFilterInput<F1::Input>>::Remainder,
+    >>::Combined: StateFilterInput<F2::Input>,
+    F2::ValidOutput: StateFilterCombination<
+        <<F1::ValidOutput as StateFilterCombination<
+            <F0::ValidOutput as StateFilterInput<F1::Input>>::Remainder,
+        >>::Combined as StateFilterInput<F2::Input>>::Remainder,
+    >,
+    <F2::ValidOutput as StateFilterCombination<
+        <<F1::ValidOutput as StateFilterCombination<
+            <F0::ValidOutput as StateFilterInput<F1::Input>>::Remainder,
+        >>::Combined as StateFilterInput<F2::Input>>::Remainder,
+    >>::Combined: StateFilterInput<F3::Input>,
+    F3::ValidOutput: StateFilterCombination<
+        <<F2::ValidOutput as StateFilterCombination<
+            <<F1::ValidOutput as StateFilterCombination<
+                <F0::ValidOutput as StateFilterInput<F1::Input>>::Remainder,
+            >>::Combined as StateFilterInput<F2::Input>>::Remainder,
+        >>::Combined as StateFilterInput<F3::Input>>::Remainder,
+    >,
 {
     type Input = F0::Input;
-    type ValidOutput = F3::ValidOutput;
+    type ValidOutput = <F3::ValidOutput as StateFilterCombination<
+        <<F2::ValidOutput as StateFilterCombination<
+            <<F1::ValidOutput as StateFilterCombination<
+                <F0::ValidOutput as StateFilterInput<F1::Input>>::Remainder,
+            >>::Combined as StateFilterInput<F2::Input>>::Remainder,
+        >>::Combined as StateFilterInput<F3::Input>>::Remainder,
+    >>::Combined;
     fn filter(state: &State, value: Self::Input) -> Option<Self::ValidOutput> {
         F0::filter(state, value)
             .and_then(|v| {
@@ -86,7 +108,7 @@ where
                 F3::filter(state, input).map(|v| v.combine(remainder))
             })
     }
-}*/
+}
 
 pub trait StateFilterInput<T>: Sized {
     type Remainder;
@@ -123,107 +145,3 @@ impl<T, U> StateFilterCombination<T> for U {
         (self, value)
     }
 }
-
-/*macro_rules! impl_state_filter_input_single_remainder {
-    (
-        $($t:ident),* $(,)? |
-        $($indexes:tt),* $(,)? |
-        $remainder_index:tt
-    ) => {
-        impl<$($t,)* R> StateFilterInput<($($t),*)> for ($($t,)* R) {
-            type Remainder = R;
-            fn new(input: ($($t),*), remainder: Self::Remainder) -> Self {
-                ($(input.$indexes,)* remainder)
-            }
-            fn split_take(self) -> (($($t),*), Self::Remainder) {
-                (($(self.$indexes),*), self.$remainder_index)
-            }
-        }
-    };
-}
-
-impl_state_filter_input_single_remainder!(T0, T1 | 0, 1 | 2);
-impl_state_filter_input_single_remainder!(T0, T1, T2 | 0, 1, 2 | 3);
-impl_state_filter_input_single_remainder!(T0, T1, T2, T3 | 0, 1, 2, 3 | 4);
-
-macro_rules! impl_state_filter_input_single_remainder_reverse {
-    (
-        $($t:ident),* $(,)? |
-        $($indexes:tt),* $(,)? |
-        $($init_indexes:tt),* $(,)?
-    ) => {
-        impl<$($t,)* R> StateFilterInput<($($t),*)> for (R $(,$t)*) {
-            type Remainder = R;
-            fn new(input: ($($t),*), remainder: Self::Remainder) -> Self {
-                (remainder $(,input.$init_indexes)*)
-            }
-            fn split_take(self) -> (($($t),*), Self::Remainder) {
-                (($(self.$indexes),*), self.0)
-            }
-        }
-    };
-}
-
-impl_state_filter_input_single_remainder_reverse!(T0, T1 | 1, 2 | 0, 1);
-impl_state_filter_input_single_remainder_reverse!(T0, T1, T2 | 1, 2, 3 | 0, 1, 2);
-impl_state_filter_input_single_remainder_reverse!(T0, T1, T2, T3 | 1, 2, 3, 4 | 0, 1, 2, 3);
-
-macro_rules! impl_state_filter_input {
-    (
-        $($t:ident),* $(,)? |
-        $($remainder:ident),* $(,)? |
-        $($indexes:tt),* $(,)? |
-        $($remainder_indexes:tt),* $(,)? |
-        $($init_remainder_indexes:tt),* $(,)?
-    ) => {
-        impl<$($t,)* $($remainder),*> StateFilterInput<($($t),*)> for ($($t,)* $($remainder),*) {
-            type Remainder = ($($remainder),*);
-            fn new(input: ($($t),*), remainder: Self::Remainder) -> Self {
-                ($(input.$indexes,)* $(remainder.$init_remainder_indexes,)*)
-            }
-            fn split_take(self) -> (($($t),*), Self::Remainder) {
-                (($(
-                    self.$indexes
-                ),*), ($(
-                    self.$remainder_indexes
-                ),*))
-            }
-        }
-    };
-}
-
-impl_state_filter_input!(T0, T2 | R0, R1 | 0, 1 | 2, 3 | 0, 1);
-impl_state_filter_input!(T0, T2, T3 | R0, R1 | 0, 1, 2 | 3, 4 | 0, 1);
-impl_state_filter_input!(T0, T1, T2, T3 | R0, R1 | 0, 1, 2, 3 | 4, 5 | 0, 1);
-impl_state_filter_input!(T0, T1, T2 | R0, R1, R2 | 0, 1, 2 | 3, 4, 5 | 0, 1, 2);
-impl_state_filter_input!(T0, T1 | R0, R1, R2, R3 | 0, 1 | 2, 3, 4, 5 | 0, 1, 2, 3);
-
-macro_rules! impl_state_filter_input_reverse {
-    (
-        $($t:ident),* $(,)? |
-        $($remainder:ident),* $(,)? |
-        $($indexes:tt),* $(,)? |
-        $($remainder_indexes:tt),* $(,)? |
-        $($init_remainder_indexes:tt),* $(,)?
-    ) => {
-        impl<$($remainder,)* $($t),*> StateFilterInput<($($remainder),*)> for ($($remainder,)* $($t),*) {
-            type Remainder = ($($t),*);
-            fn new(input: ($($remainder),*), remainder: Self::Remainder) -> Self {
-                ($(input.$remainder_indexes,)* $(remainder.$init_remainder_indexes,)*)
-            }
-            fn split_take(self) -> (($($remainder),*), Self::Remainder) {
-                (($(
-                    self.$remainder_indexes
-                ),*), ($(
-                    self.$indexes
-                ),*))
-            }
-        }
-    };
-}*/
-
-//impl_state_filter_input_reverse!(T0, T2 | R0, R1 | 0, 1 | 2, 3 | 0, 1);
-//impl_state_filter_input_reverse!(T0, T2, T3 | R0, R1 | 0, 1, 2 | 3, 4 | 0, 1);
-//impl_state_filter_input_reverse!(T0, T1, T2, T3 | R0, R1 | 0, 1, 2, 3 | 4, 5 | 0, 1);
-//impl_state_filter_input_reverse!(T0, T1, T2 | R0, R1, R2 | 0, 1, 2 | 3, 4, 5 | 0, 1, 2);
-//impl_state_filter_input_reverse!(T0, T1 | R0, R1, R2, R3 | 0, 1 | 2, 3, 4, 5 | 0, 1, 2, 3);
